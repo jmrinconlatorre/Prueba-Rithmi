85e0fb8c0c163a0105931d15d021149c9e5db594
jmrinconlatorre <jm.rincon.latorre@gmail.com>
2020-04-13 20:58:03 +0200

    [ADD] added get getDataFromApiAsync and first render in App.js

--- App.js
+++ App.js
@@ -7,16 +7,81 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, SectionList } from 'react-native';
+import { StyleSheet, View, Text, SectionList, SafeAreaView } from 'react-native';
 
 class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			loading: false,
+			data: [],
+			endPoint: 'https://rithmi-frontend-test.s3-eu-west-1.amazonaws.com/samples.json'
+		};
+
+		this.getDataFromApiAsync = this.getDataFromApiAsync.bind(this);
+	}
+
+	componentDidMount() {
+		this.getDataFromApiAsync();		
+	}
+
+	async getDataFromApiAsync() {
+		try {
+			let response = await fetch(this.state.endPoint);
+			let json = await response.json();
+
+			this.setState({
+				data: json,
+				loading: false
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 	render() {
-		return null;
+		const { data, loading } = this.state;
+
+		if (loading) {
+			return (
+				<View style={styles.container}>
+					<Text>Connecting</Text>
+				</View>
+			);
+		}
+		
+		return (
+			<View style={styles.container}>
+				<Text style={styles.text}>Listado de frecuencias </Text>
+			</View>
+		);
 	}
 }
 
 const styles = StyleSheet.create({
-  
+	container: {
+		flex: 1,
+		backgroundColor: '#2980b9',
+		justifyContent: 'center'
+	},
+	text: {
+		fontSize: 40,
+		color: '#FFF',
+		fontWeight: 'bold'
+	},
+	item: {
+		backgroundColor: '#f9c2ff',
+		padding: 20,
+		marginVertical: 8
+	},
+	header: {
+		fontSize: 32,
+		backgroundColor: '#000'
+	},
+	title: {
+		fontSize: 24
+	}
 });
 
 export default App;
